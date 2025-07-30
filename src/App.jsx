b79b1687a7ef492ab1c227fadd595b622bc487e8
ali <alihasansaad1@gmail.com>
2025-07-30 13:10:24 +0300

    message describing code changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
+import './App.css';
+import { useState } from 'react';
+export const availableIngredients = [
+  { name: 'Kaiser Bun', color: 'saddlebrown' },
+  { name: 'Sesame Bun', color: 'sandybrown' },
+  { name: 'Gluten Free Bun', color: 'peru' },
+  { name: 'Lettuce Wrap', color: 'olivedrab' },
+  { name: 'Beef Patty', color: '#3F250B' },
+  { name: 'Soy Patty', color: '#3F250B' },
+  { name: 'Black Bean Patty', color: '#3F250B' },
+  { name: 'Chicken Patty', color: 'burlywood' },
+  { name: 'Lettuce', color: 'lawngreen' },
+  { name: 'Tomato', color: 'tomato' },
+  { name: 'Bacon', color: 'maroon' },
+  { name: 'Onion', color: 'lightyellow' },
+  { name: 'Cheddar Cheese', color: '#FDE18B' },
+  { name: 'Swiss Cheese', color: '#F1E1A8' },
+];
+const App = () => {
+  const [stack, setStack] = useState([])
+  const addToBurger = (ingredients) => {
+    setStack([ingredients, ...stack])
+  }
+  const removeFromBurger = (index) => {
+    const newStack = []
 
-function App() {
-  const [count, setCount] = useState(0)
+    for (let i = 0; i < stack.length; i++) {
+      if (i !== index) {
+        newStack.push(stack[i])
+      }
+    }
+    setStack(newStack)
+  }
+    return (
+      <main>
+        <h1>Burger Stacker</h1>
+        <section>
+          <>
+            <h2>Ingredients</h2>
+            <br></br>
+            <ul>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+              {availableIngredients.map((ingredients, index) => {
+                return(
 
-export default App
+                  <li style={{ backgroundColor: ingredients.color }} key={index}>name
+                  : {ingredients.name}
+                  <button onClick={() => addToBurger(ingredients)}>+</button>
+                </li>
+                )
+              })}
+            </ul>
+          </>
+          <>
+            <h2>Your choices</h2>
+            {stack.length === 0 ? (<p> no ingredients</p>) : (            
+                <ul>
+                {stack.map((ingredients, index) => {
+                  <li key={index} style={{ backgroundColor: ingredients.color }}>
+                    name {ingredients.name}
+                    <button onClick={() => removeFromBurger(index)}>-</button>
+                  </li>
+                })}
+              </ul>
+            )}
+          </>
+        </section>
+      </main>
+    );
+  };
+
+export default App;
+
+
